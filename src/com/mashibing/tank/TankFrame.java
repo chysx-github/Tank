8e21fd7011614b4581cec98fcc4c4a79d549c7c1
chysx <wchysx@gmail.com>
2019-12-11 02:12:47 +0800

    第七次_提交：消除子弹列表内存泄漏问题，迭代器删除问题还未理解

--- src/com/mashibing/tank/TankFrame.java
+++ src/com/mashibing/tank/TankFrame.java
@@ -8,12 +8,15 @@ import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TankFrame extends Frame {
     Tank myTank = new Tank(200,200, Dir.DOWN,this);
-    Bullet b = new Bullet(300,300, Dir.DOWN);
+    List<Bullet> bullets = new ArrayList<>();
 
     static final int GAME_WIDTH = 800, GAME_HEIGHT = 600;
+
     //构造方法
     public TankFrame(){
         // 下面不写this也可以
@@ -52,9 +55,15 @@ public class TankFrame extends Frame {
 
     @Override
     public void paint(Graphics g) {
+        Color c = g.getColor();
+        g.setColor(Color.WHITE);
+        g.drawString("子弹的数量:" + bullets.size(), 10, 60);
+        g.setColor(c);
 
         myTank.paint(g);
-        b.paint(g);
+        for(int i=0; i<bullets.size(); i++) { //为什么不会出错？存疑
+            bullets.get(i).paint(g);
+        }
     }
 
     //内部类,键盘监听类
